9b134a636898bbe8194559ed9cac60744833035a
chimiseanga <seang696@gmail.com>
2026-01-15 17:09:38 -0800

    Added event schema

---
+++ schemaTypes/event.js
@@ -0,0 +1,49 @@
+export default {
+  name: 'event',
+  title: 'Event',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'title' },
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'startDate',
+      title: 'Start date',
+      type: 'datetime',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'endDate',
+      title: 'End date',
+      type: 'datetime'
+    },
+    {
+      name: 'location',
+      title: 'Location',
+      type: 'string'
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [{ type: 'block' }]
+    }
+  ],
+  orderings: [
+    {
+      title: 'Start date (soonest first)',
+      name: 'startDateAsc',
+      by: [{ field: 'startDate', direction: 'asc' }]
+    }
+  ]
+}
